62364dc1cf013d1ff8b3fd041e43bcf4a088a1bf
aran8276 <aran8276@gmail.com>
2025-06-08 01:25:01 +0700

    implement monitoring / cctv data

--- src/page/Monitoring/Monitoring.type.ts
+++ src/page/Monitoring/Monitoring.type.ts
@@ -1,12 +1,27 @@
 import { CCTV } from "@/server-actions/CCTV/CCTV.type";
+import { Dispatch, FormEvent, SetStateAction } from "react";
 
 export interface MonitoringViewProps {
+  postCCTV: (e: FormEvent<HTMLFormElement>) => Promise<void>;
+  deleteCCTV: (id: string) => Promise<void>;
   cctvs: CCTV[];
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
 }
 
 export interface PostCCTVResponse {
   message: string;
   cctv: Cctv;
+}
+
+export interface DeleteCCTVResponse {
+  message: string;
+  cctv: Cctv;
+}
+
+export interface Cctv {
+  cctv_id: string;
+  location: string;
 }
 
 export interface Cctv {
